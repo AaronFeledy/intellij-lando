a9476694b1cbd4488fc06bd2344948481b86562f
GitHub Action <action@github.com>
2022-04-20 14:51:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/aaronfeledy/landointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aaronfeledy.landointellijplugin.services
+
+import com.github.aaronfeledy.landointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/aaronfeledy/landointellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aaronfeledy/landointellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aaronfeledy.landointellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aaronfeledy.landointellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
