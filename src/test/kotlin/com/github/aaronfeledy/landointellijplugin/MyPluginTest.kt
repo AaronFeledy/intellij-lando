4e7cfbf4d5586159e2a1197ec497509c92d0a787
Aaron Feledy <aaron@arrowonesolutions.com>
2023-12-10 13:33:21 -0500

    Clean up jetbrains template artifacts

--- src/test/kotlin/com/github/aaronfeledy/landointellijplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/aaronfeledy/landointellijplugin/MyPluginTest.kt
@@ -1,31 +1,16 @@
 package com.github.aaronfeledy.landointellijplugin
 
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.psi.xml.XmlFile
+import com.github.aaronfeledy.landointellijplugin.services.LandoProjectService
+import com.intellij.openapi.components.service
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.util.PsiErrorElementUtil
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
-    fun testXMLFile() {
-        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+    fun testProjectService() {
+        val projectService = project.service<LandoProjectService>()
 
-        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
-
-        assertNotNull(xmlFile.rootTag)
-
-        xmlFile.rootTag?.let {
-            assertEquals("foo", it.name)
-            assertEquals("bar", it.value.text)
-        }
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
+        assertNotNull(projectService)
     }
 }
