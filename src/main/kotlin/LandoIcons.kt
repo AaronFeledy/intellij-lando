78f085a96d9cbb7b088298c68fade81387750f55
Aaron Feledy <aaron@arrowonesolutions.com>
2023-12-15 18:51:15 -0500

    Move icons object into an icons package

--- src/main/kotlin/LandoIcons.kt
+++
@@ -1,7 +0,0 @@
-package icons
-
-import com.intellij.openapi.util.IconLoader
-
-object LandoIcons {
-    val Lando_default_icon = IconLoader.getIcon("/icons/lando_16.svg", LandoIcons.javaClass)
-}
---
+++ src/main/kotlin/icons/LandoIcons.kt
@@ -0,0 +1,7 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+
+class LandoIcons {
+    val default = IconLoader.getIcon("/icons/lando_16.svg", LandoIcons::class.java)
+}
