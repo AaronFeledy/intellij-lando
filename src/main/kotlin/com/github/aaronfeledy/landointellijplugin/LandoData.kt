56a371944b169a0c347054ed3f5e07e81c4fbb56
Aaron Feledy <aaron@arrowonesolutions.com>
2024-02-28 12:51:41 -0500

    servicedata class

--- src/main/kotlin/com/github/aaronfeledy/landointellijplugin/LandoData.kt
+++ src/main/kotlin/com/github/aaronfeledy/landointellijplugin/LandoData.kt
@@ -1,14 +1,30 @@
 package com.github.aaronfeledy.landointellijplugin
 
+/**
+ * Data class representing the service data for a Lando application.
+ *
+ * @property service The name of the service.
+ * @property urls The list of URLs associated with the service.
+ * @property type The type of the service.
+ * @property version The version of the service.
+ * @property via The name of the service that hosts this service.
+ * @property webroot The webroot directory for the service.
+ * @property config The configuration map for the service.
+ * @property healthy The health status of the service.
+ * @property meUser The user associated with the service.
+ * @property hasCerts A boolean indicating whether the service has certificates.
+ * @property api The Lando API version used to build the service.
+ * @property hostnames The list of hostnames associated with the service.
+ */
 data class ServiceData(
     val service: String,
     val urls: List<String>,
     val type: String,
-    val healthy: String,
+    val version: String,
     val via: String,
     val webroot: String,
     val config: Map<String, Any>,
-    val version: String,
+    val healthy: String,
     val meUser: String,
     val hasCerts: Boolean,
     val api: Int,
